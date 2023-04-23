6b4e4eefe10ac9bea3e0d6a91bb1cba95dc1cfe1
tzhangm <tzhangm@trip.com>
2023-04-23 23:02:06 +0800

    upd

--- src/pages/StarDetail/StarCoreInfo/FlowValueTabItem.tsx
+++ src/pages/StarDetail/StarCoreInfo/FlowValueTabItem.tsx
@@ -17,6 +17,44 @@ const option = {
         }
     ]
 };
+
+
+// 六芒星
+const option_liu = {
+    title: {
+        text: 'Basic Radar Chart'
+    },
+    legend: {
+        data: ['Allocated Budget', 'Actual Spending']
+    },
+    radar: {
+        // shape: 'circle',
+        indicator: [
+            { name: 'Sales', max: 6500 },
+            { name: 'Administration', max: 16000 },
+            { name: 'Information Technology', max: 30000 },
+            { name: 'Customer Support', max: 38000 },
+            { name: 'Development', max: 52000 },
+            { name: 'Marketing', max: 25000 }
+        ]
+    },
+    series: [
+        {
+            name: 'Budget vs spending',
+            type: 'radar',
+            data: [
+                {
+                    value: [4200, 3000, 20000, 35000, 50000, 18000],
+                    name: 'Allocated Budget'
+                },
+                {
+                    value: [5000, 14000, 28000, 26000, 42000, 21000],
+                    name: 'Actual Spending'
+                }
+            ]
+        }
+    ]
+};
 const FlowValueTabItem = () => {
     const token = useToken()
     return <div>
@@ -37,6 +75,16 @@ const FlowValueTabItem = () => {
             lazyUpdate={true}
             theme={"theme_name"}
         />
+
+
+        {/*六芒星*/}
+        <ReactECharts
+            option={option_liu}
+            notMerge={true}
+            lazyUpdate={true}
+            theme={"theme_name"}
+        />
+
     </div>
 }
 
